1d6ecca5d308706bcf0d72892f6a7dffb708c9cc
dvtinh99 <tinh.doan@codecomplete.jp>
2025-05-05 07:32:53 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="bg-[#06121E]">
-    <AppHeader />
-    <slot />
-    <AppFooter />
+  <div>
+      <slot />
   </div>
 </template>
 <script setup lang="ts">
-import { useInitData } from '~/composables/useInitData'
-useInitData()
 </script>
